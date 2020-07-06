fbbd3eecb83e4d2a462c16f26c1c79b5f4782ec8
bashir2017 <bashiralhanshali@yahoo.com>
2020-07-05 21:33:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+
+function map(array, fn){
+    let newArray = []
+    for(let i =0; i<array.length; i++){
+        newArray.push(fn(array[i]))
+    }
+    return newArray
+}
+
+
+
+function reduce(array, fN, startingValue) {
+    let value = null;
+    if (startingValue === undefined) {
+      value = array[0];
+    } else {
+      value = startingValue;
+    }
+    let i = startingValue ? 0 : 1;
+    while (i < array.length) {
+      value = fN(array[i], value);
+      i++;
+    }
+    return value;
+  }
